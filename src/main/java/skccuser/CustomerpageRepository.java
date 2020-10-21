42703cf6b046eac53aef9c95fd7f9b33956ac712
YoungDukGe1000Won <yoduli123@gmail.com>
2020-10-21 18:49:16 +0900

    1st commit..

---
+++ src/main/java/skccuser/CustomerpageRepository.java
@@ -0,0 +1,12 @@
+package skccuser;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CustomerpageRepository extends CrudRepository<Customerpage, Long> {
+
+    List<Customerpage> findByOrderId(Long orderId);
+
+}
